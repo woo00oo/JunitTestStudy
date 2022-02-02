2a37439708b758188532fca4978f0ae5930c9c89
woo00oo <96woo94@naver.com>
2022-02-02 19:38:41 +0900

    더 자바 : JUnit5 테스트 이름 표시하기

--- src/test/java/test/junit/thejavatest/StudyTest.java
+++ src/test/java/test/junit/thejavatest/StudyTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
     @Test
-    void create() {
+    @DisplayName("스터디 만들기")
+    void create_new_study() {
         Study study = new Study();
         assertNotNull(study);
         System.out.println("create");
@@ -15,7 +17,7 @@ class StudyTest {
 
     @Test
     @Disabled //테스트를 실행하지 않음
-    void create1() {
+    void create_new_study_again() {
         System.out.println("create1");
     }
 
