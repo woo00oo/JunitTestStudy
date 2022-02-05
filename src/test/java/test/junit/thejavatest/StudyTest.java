674053893241491500cce9812740c92b9f75393f
woo00oo <96woo94@naver.com>
2022-02-05 16:43:44 +0900

    더 자바 : JUnit5 테스트 반복하기 1부

--- src/test/java/test/junit/thejavatest/StudyTest.java
+++ src/test/java/test/junit/thejavatest/StudyTest.java
@@ -1,6 +1,8 @@
 package test.junit.thejavatest;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Duration;
 
@@ -105,4 +107,17 @@ class StudyTest {
 
     }
 
+    @DisplayName("스터디 만들기")
+    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetition}")
+    void repeatTest(RepetitionInfo repetitionInfo) {
+        System.out.println("test " + repetitionInfo.getCurrentRepetition() + "/" +
+                repetitionInfo.getTotalRepetitions());
+    }
+
+    @DisplayName("스터디 만들기")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있습니다"})
+    void parameterizedTest(String message) {
+        System.out.println(message);
+    }
 }
